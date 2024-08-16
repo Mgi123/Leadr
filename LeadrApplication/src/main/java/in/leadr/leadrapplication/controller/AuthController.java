ca3832feed1f95a89b3777cb57b5628cda9b7981
unknown <leenapatil55@gmail.com>
2024-08-16 22:34:28 +0530

    admin Dashboard Created

--- LeadrApplication/src/main/java/in/leadr/leadrapplication/controller/AuthController.java
+++ LeadrApplication/src/main/java/in/leadr/leadrapplication/controller/AuthController.java
@@ -4,6 +4,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import in.leadr.leadrapplication.service.UserService;
@@ -13,10 +15,18 @@ public class AuthController {
 	    @Autowired
 	    private UserService userService;
 
+	    
 	    @GetMapping("/")
 	    public String index() {
 	        return "index.jsp";
 	    }
+	    
+	    @GetMapping("/admin")
+	    public String sample() {
+	        return "admin.jsp";
+	    }
+	   
+	   
 
 	    @PostMapping("/login")
 	    public String login(@RequestParam String username, @RequestParam String password) {
